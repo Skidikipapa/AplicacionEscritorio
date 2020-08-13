14a078c6df0995acdd3993fa3ac2668a4b50c89f
Aleix Barrera <aleix@localhost.localdomain>
2020-08-13 17:59:35 +0200

    Añadidos metodos de comprovacion y modificacion usuarios

--- mainescritorio/src/main/java/es/prueba/app/mainescritorio/rest/ClienteRest.java
+++ mainescritorio/src/main/java/es/prueba/app/mainescritorio/rest/ClienteRest.java
@@ -1,33 +1,35 @@
 package es.prueba.app.mainescritorio.rest;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class ClienteRest {
 
-  public String urlServidor;
+	public String urlServidor;
 
-  private static ClienteRest rest = null;
+	private static ClienteRest rest = null;
 
-  private ClienteRest() {
-    this.urlServidor = "http://localhost:8080/";
-  }
+	private ClienteRest() {
+		this.urlServidor = "http://localhost:8080/";
+	}
 
-  public static ClienteRest getInstancia() {
-    if (rest == null)
-      rest = new ClienteRest();
-    return rest;
-  }
+	public static ClienteRest getInstancia() {
+		if (rest == null)
+			rest = new ClienteRest();
+		return rest;
+	}
 
-  public String consultaConexion() {
-    try {
-			URL url = new URL(urlServidor);			
+	public String consultaConexion() {
+		try {
+			URL url = new URL(urlServidor);
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
 			con.setRequestMethod("GET");
-			try(InputStream respuesta = con.getInputStream();) 
-			{
+			try (InputStream respuesta = con.getInputStream();) {
 				StringBuilder resultado = new StringBuilder();
 				int c = 0;
 				while ((c = respuesta.read()) != -1) {
@@ -38,5 +40,41 @@ public class ClienteRest {
 		} catch (IOException e) {
 			return "-1";
 		}
-  }
+	}
+
+	public String añadeConexion() {
+		return modificaConexion("1");
+	}
+
+	public String reduceConexion() {
+		return modificaConexion("-1");
+	}
+
+	private String modificaConexion(String modificador) {
+		try {
+			URL url = new URL(urlServidor);
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("POST");
+			con.setRequestProperty("Content-Type", "application/json");
+			con.setDoOutput(true);
+			try (DataOutputStream wr = new DataOutputStream(con.getOutputStream());) {
+				wr.writeBytes(modificador);
+				wr.flush();
+			}
+
+			if (con.getResponseCode() == HttpURLConnection.HTTP_OK) {
+				try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));) {
+					StringBuffer response = new StringBuffer();
+					String output;
+					while ((output = in.readLine()) != null) {
+						response.append(output);
+					}
+					return response.toString();
+				}
+			}
+			else return "0";
+		} catch (IOException e) {
+			return "0";
+		}
+	}
 }
