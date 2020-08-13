c4b46422e1a323bbcb94942c84c29988c0dbf2b0
Aleix Barrera <aleixbarrera@hotmail.es>
2020-08-13 18:42:28 +0200

    Test realizados del ClienteRest

--- mainescritorio/src/test/java/es/prueba/app/mainescritorio/MainEscritorioTests.java
+++ mainescritorio/src/test/java/es/prueba/app/mainescritorio/MainEscritorioTests.java
@@ -1,38 +1,24 @@
 package es.prueba.app.mainescritorio;
 
+import es.prueba.app.mainescritorio.rest.ClienteRest;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class MainEscritorioTests 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public MainEscritorioTests( String testName )
-    {
-        super( testName );
-    }
+public class MainEscritorioTests extends TestCase {
+	public MainEscritorioTests(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( MainEscritorioTests.class );
-    }
+	public static Test suite() {
+		return new TestSuite(MainEscritorioTests.class);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	public void testApp() {
+		ClienteRest rest = ClienteRest.getInstancia();
+		assertEquals("0", rest.consultaConexion());
+		assertEquals("1", rest.añadeConexion());
+		assertEquals("2", rest.añadeConexion());
+		assertEquals("1", rest.reduceConexion());
+	}
 }
